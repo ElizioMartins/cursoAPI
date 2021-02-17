51eedfb3cfc539317534745b2efe42844bf33a27
ElizioMartins <elizio.martins@hotmail.com>
2021-02-17 15:09:57 -0300

    Primeira parte

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace cursoAPI.Controllers
+{
+    [Route("api/controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(cursoAPI.Models.Usuarios.LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+         [HttpPost]
+        public IActionResult Registrar (cursoAPI.Models.Usuarios.LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
